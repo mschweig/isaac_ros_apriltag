4b6dacf0a25bb0ba50628e4595423b70b75a6e03
Manuel Schweiger <schweigermanuel@icloud.com>
2025-02-06 08:58:23 +0100

    updated launchfile

--- isaac_ros_apriltag/launch/isaac_ros_apriltag.launch.py
+++ isaac_ros_apriltag/launch/isaac_ros_apriltag.launch.py
@@ -1,26 +1,10 @@
-# SPDX-FileCopyrightText: NVIDIA CORPORATION & AFFILIATES
-# Copyright (c) 2021-2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# SPDX-License-Identifier: Apache-2.0
-
 import launch
+from launch.actions import ExecuteProcess
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 
-
 def generate_launch_description():
+    # Define the AprilTag node
     apriltag_node = ComposableNode(
         package='isaac_ros_apriltag',
         plugin='nvidia::isaac_ros::apriltag::AprilTagNode',
@@ -34,6 +18,7 @@ def generate_launch_description():
                      'tag_family': 'tag25h9'}]
     )
 
+    # Define the container for AprilTag node
     apriltag_container = ComposableNodeContainer(
         package='rclcpp_components',
         name='apriltag_container',
@@ -45,4 +30,13 @@ def generate_launch_description():
         output='screen'
     )
 
-    return launch.LaunchDescription([apriltag_container])
+    # Launch Zenoh Router using ExecuteProcess
+    zenoh_router = ExecuteProcess(
+        cmd=['ros2', 'run', 'rmw_zenoh_cpp', 'rmw_zenohd'],
+        output='screen'
+    )
+
+    return launch.LaunchDescription([
+        apriltag_container,
+        zenoh_router
+    ])
